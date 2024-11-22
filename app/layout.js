90d36d4ab8796937a60645456e2d25d14a6c689c
William Horn <williamjosephhorn@gmail.com>
2024-11-22 13:45:23 -0500

    changed font style

--- app/layout.js
+++ app/layout.js
@@ -21,6 +21,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+        <link href="https://fonts.googleapis.com/css2?family=Arsenal+SC:ital,wght@0,400;0,700;1,400;1,700&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Oswald&display=swap" rel="stylesheet"/>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- app/page.js
+++ app/page.js
@@ -3,8 +3,20 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="">
-      <main>
-        <h1>Hello, world!</h1>
+      <header className="py-8">
+        <h1 className="text-[#ff437d] text-center font-bold text-4xl arsenal-sc-bold">Della Porta Cipher</h1>
+      </header>
+
+      <main className="flex justify-center">
+        <div className="w-[50%]">
+          <p className="font-light montserrat text-center text-lg">
+            The function of the 
+            <a 
+            className="text-[#dc51cd]"
+            href="https://sites.google.com/site/cryptocrackprogram/user-guide/cipher-types/substitution/porta">Della Porta Cipher</a> 
+            is to encrypt and decrypt a message in plain text.
+          </p>
+        </div>
       </main>
     </div>
   );
